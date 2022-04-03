614ed56c4e05a5a44ef89b15722a9949ff7f8ac0
shambhavik14 <68625339+shambhavik14@users.noreply.github.com>
2022-04-02 20:54:16 -0400

    Update profile-reducer.js

--- src/components/tuiter/reducers/profile-reducer.js
+++ src/components/tuiter/reducers/profile-reducer.js
@@ -4,42 +4,17 @@ import moment from "moment";
 const profileReducer = (state = profile, action) => {
                      switch (action.type) {
                              case 'edit-profile':
-                                 console.log(action.profile);
-                                 const newProfile = {};
-                                 if (action.profileReducer.name) {
-                                     newProfile["firstName"] = action.profileReducer["name"].split(" ")[0];
-                                     newProfile["lastName"] = action.profileReducer["name"].split(" ")[1];
-                                 }
-
-                                 if(action.profileReducer.bio){
-                                     newProfile["bio"] = action.profileReducer["bio"];
-                                 }
-
-                                 if(action.profileReducer.location){
-                                     newProfile["location"] = action.profileReducer["location"];
-                                 }
-
-                                 if(action.profileReducer.website){
-                                     newProfile["website"] = action.profileReducer["website"];
-                                 }
-
-                                 if(action.profileReducer.dateOfBirth){
-                                     console.log(action.profileReducer["dateOfBirth"]);
-                                     newProfile["dateOfBirth"] = moment(action.profileReducer["dateOfBirth"], 'Y/M/D').format('M/D/Y').toString();
-                                     console.log(newProfile["dateOfBirth"]);
-                                 }
-
-                                 return [
-                                     {
-                                         ...state[0],
-                                         // ...action.profile,
-                                         ...newProfile,
-                                     }
-                                 ];
+                             console.log(state);
+                             console.log(action.value);
+                             state[0].name = action.name;
+                             state[0].bio = action.bio;
+                             state[0].location = action.location;
+                             state[0].website = action.website;
+                             state[0].dateOfBirth = action.dateOfBirth;
+                             return state;
 
                              default:
-                                 return (state);
-
+                             return state;
                      }
                      }
 
