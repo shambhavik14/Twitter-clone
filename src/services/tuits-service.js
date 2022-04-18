aaaab1435b8e27911c82245865454202ff98a2d7
shambhavik14 <68625339+shambhavik14@users.noreply.github.com>
2022-04-17 22:08:46 -0400

    Update tuits-service.js

--- src/services/tuits-service.js
+++ src/services/tuits-service.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const TUITS_API = 'https://fullstack-mongo.herokuapp.com/api/tuits';
+const API_BASE = process.env.REACT_APP_API_BASE;
+const TUITS_API = `${API_BASE}/tuits`
 
 
 export const createTuit = async (tuits) => {
